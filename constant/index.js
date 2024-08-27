2aedb45dfb497048768c6100812f240456fdb5ad
Huy <huy.maisy@vti.com>
2024-08-27 21:37:00 +0700

    add link

--- constant/index.js
+++ constant/index.js
@@ -1,6 +1,6 @@
 const CONSTANT = {
   TIME_REPEAT_AGAIN: 24 * 60 * 60, // 24h chạy 1 lần
-  PROJECT_REPEAT: ['cats-small', 'goats', 'bool', 'ducks', 'duck-chain'],
+  PROJECT_REPEAT: ['cats-small', 'goats', 'bool', 'ducks', 'duck-chain'], // Hằng ngày sẽ chạy những con này
 };
 
 module.exports = CONSTANT
